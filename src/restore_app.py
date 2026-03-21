135c65142feff4fad6f972329de6f09946e06d2f
carlos <carlos.manuel@soltein.mx>
2026-03-20 18:13:18 -0600

    Add Snap packaging support and update installation instructions

--- src/restore_app.py
+++ src/restore_app.py
@@ -505,7 +505,7 @@ class MainWindow(QMainWindow):
 
         self._status_bar = QStatusBar()
         self.setStatusBar(self._status_bar)
-        self._status_bar.showMessage("Listo para restaurar respaldos")
+        self._status_bar.showMessage("Listo para restaurar bases Odoo")
 
     def _build_restore_tab(self) -> QWidget:
         widget = QWidget()
